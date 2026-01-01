2b81332de60c93b2296e39bdb74951bd3cb3e70e
w1201s <asidhah12123@gmail.com>
2026-01-01 08:22:16 +0700

    Update Venus Game list.lua

--- Venus Game list.lua
+++ Venus Game list.lua
@@ -1,6 +1,6 @@
 local Games = {
   [1502601752] = "https://github.com/w1201s/Script/blob/main/rabbit%20simulator%202%20oldest%20game%20in%20the%20book%20btw", --Rabbit simulator
   [1268927906] = "https://raw.githubusercontent.com/w1201s/Script/refs/heads/main/Muscle%20legends.lua", --Muscle legends
-  [9263942570] = "https://raw.githubusercontent.com/w1201s/Script/refs/heads/main/1%20click%20%3D%201%20grow%20simple%20script", --1 click = 1 grow
+  [9263942570] = "https://raw.githubusercontent.com/w1201s/Script/refs/heads/main/1%20click%20%3D%201%20grow%20simple%20script.lua", --1 click = 1 grow
 }
 return Games
