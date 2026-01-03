ec2c90bf6859297e445181cd73118df31d711e5c
w1201s <asidhah12123@gmail.com>
2026-01-03 09:18:20 +0700

    Update blox fruit main.lua

--- blox fruit main.lua
+++ blox fruit main.lua
@@ -1,7 +1,7 @@
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 
 local Window = WindUI:CreateWindow({
-    Title = "My Super Hub",
+    Title = "My Super Hdadaadadub",
     Icon = "door-open", -- lucide icon. optional
     Author = "by .ftgs and .ftgs", -- optional
 })
